ff4abb839d8a691ec3a0af41c3e37d963670deef
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-15 21:09:03 +0900

    ファクトリーボットの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {'test@example'}
     password              {'000000'}
     password_confirmation {password}
+    birthday              {'2001-01-01'}
+    family_name           {'山田'}
+    first_name            {'太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'タロウ'}
   end
 end
