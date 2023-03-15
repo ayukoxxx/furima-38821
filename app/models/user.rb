322eb9c607820071f2aec3a3b117d011d6e15fac
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-15 20:54:10 +0900

    バリデーションの設定・エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,18 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :birthday, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  validates :password, format: {with:/[a-z\d]{6,}/i, message:'半角英数を含む６文字以上を入力してください'}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  
+
+  with_options presence: true, format: { with: /\A[ァ-ヴ一]+\z/u, message: '全角カタカナを使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
 end
