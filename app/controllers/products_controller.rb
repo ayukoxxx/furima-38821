2dfbb43b2a92b77e4d5793094a20db3ec60793fe
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-17 19:55:20 +0900

    activehashの記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
     @user = User.new
+    @products = Product.order("created_at DESC")
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,14 +3,23 @@ class Product < ApplicationRecord
 validates :image, presence: true
 validates :name, presence: true
 validates :price, presence: true
-validates :shipping_cost_id, presence: true
-validates :condition_id, presence: true
-validates :area_id, presence: true
-validates :ship_date_id, presence: true
+validates :shipping_cost_id,  numericality: { other_than: 1 , message: "can't be blank"} 
+validates :condition_id,  numericality: { other_than: 1, message: "can't be blank" } 
+validates :area_id,  numericality: { other_than: 1 , message: "can't be blank"} 
+validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" } 
 validates :introduction, presence: true
-validates :category_id, presence: true
+validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 
 belongs_to :user
 has_one_attached :image
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_cost
+  belongs_to :area
+  belongs_to :ship_date
+  belongs_to :category
+  belongs_to :condition
+
+
 end
