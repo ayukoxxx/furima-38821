20f9f202dcacbd80d2a6a1ea14b9fe1eaf28c0bf
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-17 15:26:41 +0900

    バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ApplicationRecord
 
+validates :image, presence: true
+validates :name, presence: true
+validates :price, presence: true
+validates :shipping_cost_id, presence: true
+validates :condition_id, presence: true
+validates :area_id, presence: true
+validates :ship_date_id, presence: true
+validates :introduction, presence: true
+validates :category_id, presence: true
+
+
 belongs_to :user
 has_one_attached :image
 end
