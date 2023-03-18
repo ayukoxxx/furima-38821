028b3ecc652b64adb0850f07e1c8e1c2c813a381
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-18 22:21:16 +0900

    javascriptの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+require('tax')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
