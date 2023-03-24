bb88931c30670aca4acbba840c0c20649113b57b
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-24 15:03:35 +0900

    データがある状態で商品の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_055625) do
+ActiveRecord::Schema.define(version: 2023_03_24_055650) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
