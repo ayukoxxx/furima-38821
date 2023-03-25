96e6fef11c51eae2a5d6bcba200ab72c6e9a673e
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-25 18:55:11 +0900

    formオブジェクトの実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,6 +1,31 @@
 class PurchaseRecordsController < ApplicationController
 
   def index
+    
+  end
+
+  def new
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postcode, :area_id, :municipality, :address, :building_name, :price, :telephone_number).merge(user_id: current_user.id, product_id: current_product.id)
   end
 
 end
+
+
+
+
