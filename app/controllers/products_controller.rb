7b671624fd2c597de3ebf7c6dbb91b0614cab802
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-25 15:50:53 +0900

    繰り返される記述をメソッドでまとめる、戻るボタンのパス変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :product_find, except:[:index,:create,:new]
 
   def index
     @products = Product.includes(:user).order("created_at DESC")
@@ -19,20 +20,18 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    
   end
 
   def edit
-    @product = Product.find(params[:id])
+
     redirect_to root_path unless current_user == @product.user
   end
 
   def update
-    product = Product.find(params[:id])
-    if product.update(product_params)
-    redirect_to  product_path(product.id)
+    if @product.update(product_params)
+    redirect_to  product_path(@product.id)
     else
-      @product = Product.find(params[:id])
       @product.update(product_params)
     render 'edit'
     end
@@ -44,6 +43,8 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name,:introduction,:price,:image,:shipping_cost_id,:condition_id,:area_id,:ship_date_id,:category_id).merge(user_id: current_user.id)
   end
 
-
+  def product_find
+    @product = Product.find(params[:id])
+  end
 
 end
