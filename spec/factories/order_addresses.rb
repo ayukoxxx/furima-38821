48b3ea69956e40447f58f6dc083b9ca3cb048a67
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-04-13 14:59:09 +0900

    order - factorybotの作成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_address do
     postcode { '123-4567' }
-    prefecture { 1 }
-    city { '東京都' }
+    area { Area.all.sample }
+    municipality { '新宿区' }
     house_number { '1-1' }
     building_name { '東京ハイツ' }
-    price { 2000 }
+    telephone_number { 0123456789 }
     
   end
 end
